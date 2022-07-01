a6c468f0977b925a50b33add427eafa473cc7d83
Alexander Khlyntcev <khlyntcevsasha@gmail.com>
2022-07-01 21:53:30 +0300

    Diploma completed

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,76 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private final Map<String, List<PageEntry>> index = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        File[] listPdfFiles = getListFiles(pdfsDir, ".pdf");
+        if (listPdfFiles != null) {
+            for (File pdfFile : listPdfFiles) {
+                scan(pdfFile);
+            }
+        }
+    }
+
+    private void scan(File pdfFile) throws IOException {
+        try (var doc = new PdfDocument(new PdfReader(pdfFile))) {
+            int numberOfPages = doc.getNumberOfPages();
+            for (int i = 1; i <= numberOfPages; i++) {
+                PdfPage page = doc.getPage(i);
+                String textFromPage = PdfTextExtractor.getTextFromPage(page);
+                var words = textFromPage.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> wordFrequency = new HashMap<>();
+                for (var word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    wordFrequency.put(word.toLowerCase(),
+                            wordFrequency.getOrDefault(word.toLowerCase(), 0) + 1);
+                }
+                for (var entry : wordFrequency.entrySet()) {
+                    PageEntry pageEntry = new PageEntry(pdfFile.getName(), i, entry.getValue());
+                    addPageEntryToIndex(index, entry.getKey(), pageEntry);
+                }
+            }
+        }
     }
 
     @Override
-    public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+    public List<PageEntry> search(@NotNull String word) {
+        var pageEntryList = index.get(word.toLowerCase());
+        Collections.sort(pageEntryList);
+        return pageEntryList;
+    }
+
+    private File @Nullable [] getListFiles(@NotNull File pdfsDir, String typeOfFile) {
+        File[] tempListFiles = pdfsDir.listFiles();
+        if (tempListFiles == null) return null;
+        List<File> files = new ArrayList<>();
+        for (File file : tempListFiles) {
+            if (file.isDirectory()) {
+                File[] listFiles = getListFiles(file, typeOfFile);
+                if (listFiles != null) files.addAll(List.of(listFiles));
+            } else {
+                if (file.getName().toLowerCase().endsWith(typeOfFile)) {
+                    files.add(file);
+                }
+            }
+        }
+        return files.toArray(new File[0]);
+    }
+
+    private void addPageEntryToIndex(@NotNull Map<String, List<PageEntry>> index, String word, PageEntry pageEntry) {
+        var pageEntryList = index.getOrDefault(word, new ArrayList<>());
+        pageEntryList.add(pageEntry);
+        index.put(word, pageEntryList);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,43 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+    public static void main(String[] args) throws IOException {
+        File srcDir = new File("pdfs");
+        System.out.println("Индексирование pdf файлов в папке " + srcDir.getPath());
+        var engine = new BooleanSearchEngine(srcDir);
+        System.out.println("Запуск сервера");
+        startServer(engine);
+    }
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+    private static void startServer(BooleanSearchEngine engine) {
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter out = new PrintWriter(socket.getOutputStream())) {
+                    String word = in.readLine();
+                    var pageEntryList = engine.search(word);
+
+                    Gson gson = new GsonBuilder()
+                            .setPrettyPrinting()
+                            .create();
+
+                    String jsonPageEntryList = gson.toJson(pageEntryList,
+                            new TypeToken<List<PageEntry>>() {}.getType());
+
+                    out.println(jsonPageEntryList);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка запуска сервера");
+            e.printStackTrace();
+        }
     }
 }
